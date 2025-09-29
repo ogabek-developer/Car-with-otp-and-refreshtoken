df5243c0c3590503672f97100d9bb634d87595ce
cyber-dev1 <ogabek200825@gmail.com>
2025-09-29 19:25:37 +0500

    Aded fix bag

--- src/lib/jwt.config.js
+++ src/lib/jwt.config.js
@@ -3,7 +3,7 @@
 import jwt from "jsonwebtoken" ;
 
 const JwtConfig = {
-    createAccessToken : (payload) => jwt.sign(payload, process.env.MY_TOKEN_KEY, {expiresIn : "5s"}) ,
+    createAccessToken : (payload) => jwt.sign(payload, process.env.MY_TOKEN_KEY, {expiresIn : "1d"}) ,
     verifyAccessToken : (token) => jwt.verify(token, process.env.MY_TOKEN_KEY),
     createRefreshToken : (payload) => jwt.sign(payload, process.env.MY_REFRESH_TOKEN_KEY, {expiresIn : "30d"}),
     verifyRefreshToken : (refreshToken) => jwt.verify(refreshToken, process.env.MY_REFRESH_TOKEN_KEY)
